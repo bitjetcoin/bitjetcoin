912c3e94810f2cf9261be8467a9e45c0474d71fd
bitjetdev <bitjetdev@gmail.com>
2018-12-24 21:05:54 +0300

    f

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -113,7 +113,7 @@ public:
         pchMessageStart[3] = 0xd0;
         nDefaultPort = 8778;
         nPruneAfterHeight = 100000;
-
+/*
         FILE * pFile;
         pFile = fopen ("c:\Bitjet\log.log","w");
 
@@ -141,10 +141,10 @@ public:
                                     //fprintf(pFile, "time = %s\n", genesis.nTime );
                                     fprintf(pFile, " hash = %s\n", bestGenesis.GetHash().ToString().c_str());
                                     fprintf(pFile, "merklehash: = %s\n", bestGenesis.hashMerkleRoot.ToString().c_str());
-
-        genesis = CreateGenesisBlock(1545670052, 1172111, 0x1f0fffff, 1, 50 * COIN);
+*/
+        genesis = CreateGenesisBlock(1545670052, 2433186, 0x1f0fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000f8a02a3cd95e5600dfbb40201e2ffa3a1b2810dd36b03051622e173055"));
+        assert(consensus.hashGenesisBlock == uint256S("0x000001fcfe00f58953ddfd5544ab3d1fa08f603a0c159609a032160ad92d9232"));
         assert(genesis.hashMerkleRoot == uint256S("0x473c82679bc7f74c27498a06fc3530d20d943a9a813687888d2f57067dee57de"));
 
         /*genesis = CreateGenesisBlock(1545320476, 3037916969, 0x1d00ffff, 1, 50 * COIN);
