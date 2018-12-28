2af7381234463eb9c32e5c671aef2ecf1b49a2f2
bitjetdev <bitjetdev@gmail.com>
2018-12-28 22:59:45 +0300

    f

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -79,6 +79,7 @@ public:
         consensus.powLimit = uint256S("00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 60 * 60; // one hour
         consensus.nPowTargetSpacing = 10; // 10 sec
+        
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 342; // 95% of 360
