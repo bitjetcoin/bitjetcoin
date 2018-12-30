ebf0573aca1f0bae5e29cc08fce46536026ddb0b
bitjetdev <bitjetdev@gmail.com>
2018-12-30 22:04:51 +0300

    f

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -447,13 +447,13 @@ static UniValue getblocktemplate(const JSONRPCRequest& request)
 
     if(!g_connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
-    /*
+
     if (g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0)
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "Bitjet is not connected!");
 
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Bitjet is downloading blocks...");
-    */
+
 
     static unsigned int nTransactionsUpdatedLast;
 
